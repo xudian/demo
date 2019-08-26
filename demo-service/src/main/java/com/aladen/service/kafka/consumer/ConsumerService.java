34c0baafdd1b68a04933d54ade885fb9bf3ebd91
dian.xu <dian.xu@china.zhaogang.com>
2019-08-26 18:04:46 +0800

    feat: kafka手动提交的2种方式

--- demo-service/src/main/java/com/aladen/service/kafka/consumer/ConsumerService.java
+++ demo-service/src/main/java/com/aladen/service/kafka/consumer/ConsumerService.java
@@ -1,5 +1,7 @@
 package com.aladen.service.kafka.consumer;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +14,24 @@ public class ConsumerService {
     private Logger logger = LoggerFactory.getLogger(ConsumerService.class);
 
 
-    @KafkaListener(topics = "kafka-demo", groupId = "group-id", containerFactory = "kafkaListenerContainerFactory")
-    public void consumer(ConsumerRecord<String,String> record, Acknowledgment ack) {
+    /**
+     *  使用 Acknowledgment 或者使用 consumer 效果是一样的，都需要手动去提交信息
+     * @param record
+     * @param consumer
+     */
+    @KafkaListener(topics = "kafka-demo", groupId = "group-id")
+    public void consumer(ConsumerRecord<String,String> record, Consumer<String,String> consumer) {
         logger.info("topic-first get topic : {}, message:{}", record.topic(), record.value());
         logger.info("partition:{};offset:{}", record.partition(), record.offset());
+        consumer.commitSync();
+    }
+
+    @KafkaListener(topics = {"topic-demo"}, groupId = "topic-id", containerFactory = "kafkaListenerContainerFactory")
+    public void consumer(ConsumerRecord<String,String> record, Acknowledgment ack) {
+        logger.info("topic:{}", record.topic());
+        logger.info("partition:{}", record.partition());
+        logger.info("offset:{}", record.offset());
+        logger.info("value:{}", record.value());
         ack.acknowledge();
     }
 }
